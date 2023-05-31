73e28c7f4f0ffeba7c9f89c98cf84f45db7ce64a
naufaldawam <nauma625@gmail.com>
2023-05-31 16:56:14 +0700

    coba setting vite agar musik masuk kedalam file vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,48 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  plugins: [
+    vue(),
+    vueJsx()
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    },
+    assetsInlineLimit: 0,
+    assetsDir: 'assets',
+    outDir: 'dist',
+    chunkSizeWarningLimit: 2000
+  },
+  publicDir: 'public',
+  server: {
+    watch: {
+      usePolling: true
+    }
+  },
+  optimizeDeps: {
+    include: ['@fortawesome/fontawesome-free/css/all.css']
+  },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.(mp3|wav)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'assets/musik'
+              }
+            }
+          ]
+        }
+      ]
     }
   }
-})
+});
