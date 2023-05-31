308a92689f17e4dc1c79165f1bb6221afa34b06c
naufaldawam <nauma625@gmail.com>
2023-05-31 17:22:09 +0700

    update setting vite biar musik masuk ke prod

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,18 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx()
-  ],
+  plugins: [vue()],
   build: {
     rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
+      input: {
+        main: './index.html',
+      },
     },
-    assetsInlineLimit: 0,
-    assetsDir: 'assets',
-    outDir: 'dist',
-    chunkSizeWarningLimit: 2000
   },
-  publicDir: 'public',
   server: {
-    watch: {
-      usePolling: true
-    }
+    fs: {
+      strict: false,
+    },
   },
-  optimizeDeps: {
-    include: ['@fortawesome/fontawesome-free/css/all.css']
-  },
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.(mp3|wav)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[name].[ext]',
-                outputPath: 'assets/musik'
-              }
-            }
-          ]
-        }
-      ]
-    }
-  }
-});
+})
